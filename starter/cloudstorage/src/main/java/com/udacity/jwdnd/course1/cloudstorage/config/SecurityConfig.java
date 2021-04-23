f86a709cdca944a51916626db9335844544d5069
sarahyf <alfadlysarah@gmail.com>
2021-04-23 12:07:53 +0300

    moving the config package to be within cloudstorage package

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.udacity.jwdnd.course1.config;
+package com.udacity.jwdnd.course1.cloudstorage.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
